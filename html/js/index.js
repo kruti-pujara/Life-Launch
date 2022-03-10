2d0dadffb952d163871a8f5997ccd02fcb3e29e1
Kruti <krutipujara17@gmail.com>
2022-03-10 11:35:32 +0530

    slider implementation done and hamburger with jQuery done

--- html/js/index.js
+++ html/js/index.js
@@ -1,22 +1,31 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const navWrapper = document.querySelector(".nav-wrapper");
+$(document).ready(function(){
+  jQuery('.menu-toggle').click(function(){
+    jQuery('html,body').toggleClass('open-menu')
+    jQuery('.nav-wrapper').toggleClass('active')
+    jQuery('.menu-toggle').toggleClass('active')
+  });
+  jQuery('.nav-link').click(function(){
+    jQuery('.menu-toggle').removeClass('active')
+    jQuery('.nav-wrapper').removeClass('active')
+  });
 
-menuToggle.addEventListener("click",() => {
-  menuToggle.classList.toggle("active");
-  navWrapper.classList.toggle("active");
+  $('.main-slider').slick({
+    slidesToShow: 1,
+  });
 })
 
-document.querySelectorAll(".nav-link").forEach(n => n.
-  addEventListener("click", () => {
-    menuToggle.classList.remove("active");
-    navWrapper.classList.remove("active");
-}))
+// const menuToggle = document.querySelector(".menu-toggle");
+// const navWrapper = document.querySelector(".nav-wrapper");
 
-// $(document).ready(function(){
-//   jQuery('.menu-toggle').click(function(){
-//     jQuery('html,body').toggleClass('open-menu')
-//   });
+// menuToggle.addEventListener("click",() => {
+//   menuToggle.classList.toggle("active");
+//   navWrapper.classList.toggle("active");
 // })
 
+// document.querySelectorAll(".nav-link").forEach(n => n.
+//   addEventListener("click", () => {
+//     menuToggle.classList.remove("active");
+//     navWrapper.classList.remove("active");
+// }))
 
   
