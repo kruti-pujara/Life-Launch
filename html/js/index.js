c703621c11ab17595d17509d69bc23288b476f83
Kruti <krutipujara17@gmail.com>
2022-03-11 09:27:14 +0530

    slick slider implemented successfully.

--- html/js/index.js
+++ html/js/index.js
@@ -11,6 +11,36 @@ $(document).ready(function(){
 
   $('.main-slider').slick({
     slidesToShow: 1,
+    slidesToScroll:2,
+    prevArrow:'<button class="slide-arrow prev-arrow"></button>',
+    nextArrow:'<button class="slide-arrow next-arrow"></button>',
+    variableWidth:true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow:1,
+          slidesToScroll:2,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow:1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+        
   });
 })
 
